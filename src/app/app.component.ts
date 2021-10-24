5744d8d4fc77039ced0258784e8c27b7ef93ecc0
Valentin Galit <valentin7galit@gmail.com>
2021-10-24 15:21:21 +0300

    2 basics and syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+
+  // title = 'Dynamic title'
+  // number = 42
+  // arr = [1, 2, 3]
+  // obj = { a: 1, b: {c: 2} }
+  // inputValue = ''
+  // img = 'https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png'
+
+  // constructor() {
+  //   // setTimeout(() => {
+  //   //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png'
+  //   // }, 5000)
+  // }
+  //
+  // onInput(event: KeyboardEvent) {
+  //   this.inputValue = (<HTMLInputElement>event.target).value;
+  // }
+  // onBlur(str: string) {
+  //   this.inputValue = str;
+  // }
+  // onClick() {
+  //   console.log('Click!');
+  // }
+
+  // title: "Initial"
+  // onInput(event: any) {
+  //   this.title = event.target.value
+  // }
+
+  // backgroundToggle = false
+  // toggle :any = false
+
+//  15
+//   arr = [1, 1, 2, 3, 5, 8, 13]
+//   objs = [
+//     {title: 'Post 1', author: 'Valentin', comments: [
+//       {name: 'Max', text: 'lorem 1'},
+//       {name: 'Max', text: 'lorem 2'},
+//       {name: 'Max', text: 'lorem 3'},
+//     ]},
+//     {title: 'Post 2', author: 'Valentin 2', comments: [
+//         {name: 'Max 2', text: 'lorem 1'},
+//         {name: 'Max 2', text: 'lorem 2'},
+//         {name: 'Max 2', text: 'lorem 3'},
+//       ]},
+//   ]
+
+//  16
+  now: Date = new Date()
 }
