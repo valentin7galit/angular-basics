6caa9bab3ef14a65ca8a0c09e34bb619afd66e1a
valentin7galit <valentin7galit@gmail.com>
2021-11-07 17:44:16 +0200

    9 Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
 import {BrowserModule} from '@angular/platform-browser'
-import {NgModule, Provider} from '@angular/core'
+import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
-import {AuthInterceptor} from './auth.interceptor'
-
-const INTERCEPTOR_PROVIDER: Provider = {
-  provide: HTTP_INTERCEPTORS,
-  useClass: AuthInterceptor,
-  multi: true
-}
+import {FormsModule} from '@angular/forms'
+import {AboutComponent} from './about/about.component'
+import {HomeComponent} from './home/home.component'
+import {PostsComponent} from './posts/posts.component'
+import {PostComponent} from './post/post.component'
+import {AboutExtraComponent} from './about-extra/about-extra.component'
+import {AppRoutingModule} from './app-routing.module';
+import { ErrorPageComponent } from './error-page/error-page.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
+    HomeComponent,
+    PostsComponent,
+    PostComponent,
+    AboutExtraComponent,
+    ErrorPageComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [INTERCEPTOR_PROVIDER],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
