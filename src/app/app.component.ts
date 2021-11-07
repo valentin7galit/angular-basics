80328da6080d4a427ddb2baa80806cf157e0b532
valentin7galit <valentin7galit@gmail.com>
2021-11-07 10:54:16 +0200

    5 Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import {Component} from '@angular/core'
 
+export interface Post {
+  title: string
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  isVisible = true
+
+  search = ''
+  searchField = 'title'
+
+  posts: Post[] = [
+    {title: 'Beer', text: 'Самое лучшее пиво в мире'},
+    {title: 'Bread', text: 'The best bread in the world'},
+    {title: 'Javascript', text: 'The best language in the world'},
+  ]
+
+  addPost() {
+    this.posts.unshift({
+      title: 'Angular 8',
+      text: 'Vladilen Angular 8 course'
+    })
+  }
+
 }
